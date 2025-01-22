b9ceb9c63b3cfe75355633ba7450893c0750e7bd
SoravitSukkarn <soravit.sukkarn@gmail.com>
2025-01-22 10:42:39 +0700

    hotfix: missing file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,33 +2,65 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NavigationBar from "./components/NavBar";
-import Dashboard from "./pages/Dashboard";
 import Student from "./pages/Student";
 import StudentDashboard from "./pages/StudentDashboard";
 import Teacher from "./pages/Teacher";
 import TeacherDashboard from "./pages/TeacherDashboard";
 import SignIn from "./pages/SignIn";
+import { AuthProvider } from "./context/useAuth";
+import { ProtectedStudentRoute } from "./context/ProtectedStudentRoute";
+import { ProtectedTeacherRoute } from "./context/ProtectedTeacherRoute";
 
 function App() {
   return (
     <>
-      <NavigationBar />
-      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <AuthProvider>
+        <NavigationBar />
         <Routes element={<NavigationBar />}>
           <Route path="/" element={<Home />} />
 
+          {/* <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"> */}
           {/* Student */}
-          <Route path="/student" element={<Student />} />
-          <Route path="/student/dashboard" element={<StudentDashboard />} />
+          <Route
+            path="/student"
+            element={
+              <ProtectedStudentRoute>
+                <Student />
+              </ProtectedStudentRoute>
+            }
+          />
+          <Route
+            path="/student/dashboard"
+            element={
+              <ProtectedStudentRoute>
+                <StudentDashboard />
+              </ProtectedStudentRoute>
+            }
+          />
 
           {/* Teacher */}
-          <Route path="/teacher" element={<Teacher />} />
-          <Route path="/teacher/dashboard" element={<TeacherDashboard />} />
+          <Route
+            path="/teacher"
+            element={
+              <ProtectedTeacherRoute>
+                <Teacher />
+              </ProtectedTeacherRoute>
+            }
+          />
+          <Route
+            path="/teacher/dashboard"
+            element={
+              <ProtectedTeacherRoute>
+                <TeacherDashboard />
+              </ProtectedTeacherRoute>
+            }
+          />
 
           {/* SignIn */}
-          <Route path="/teacher/dashboard" element={<SignIn />} />
+          <Route path="/login" element={<SignIn />} />
+          {/* </div> */}
         </Routes>
-      </div>
+      </AuthProvider>
     </>
   );
 }
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,44 +1,53 @@
+import { useAuth } from "../context/useAuth";
+
 export default function NavigationBar() {
+  const { user, logout } = useAuth();
   return (
     <>
-      <nav class="bg-white border-gray-200 dark:bg-gray-900">
-        <div class="max-w-screen-xl flex flex-row items-center justify-between mx-auto p-4">
-          <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              PSU Registrar System
-            </span>
-          </a>
-          <div class="items-center justify-between" id="navbar-user">
-            <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
-                <a
-                  href="/"
-                  class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                  aria-current="page"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/student/dashboard"
-                  class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Student Dashboard
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/teacher/dashboard/"
-                  class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Teacher Dashboard
-                </a>
-              </li>
+      <nav className="bg-blue-950">
+        <div className="max-w-screen-xl flex flex-row items-center justify-between mx-auto p-4 ">
+          <div className="w-1/6">
+            <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                PSU Registrar System
+              </span>
+            </a>
+          </div>
+          <div className="items-center justify-between" id="navbar-user">
+            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-blue-950 md:dark:bg-blue-950 dark:border-blue-950">
+              {user && user.role == "student" && (
+                <li>
+                  <a
+                    href="/student/dashboard"
+                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  >
+                    {/* Student Dashboard */}
+                    Dashboard
+                  </a>
+                </li>
+              )}
+              {user && user.role == "teacher" && (
+                <li>
+                  <a
+                    href="/teacher/dashboard/"
+                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  >
+                    {/* Teacher Dashboard */}
+                    Dashboard
+                  </a>
+                </li>
+              )}
             </ul>
           </div>
-          <div className="px-5 py-1 bg-white rounded-full font-semibold ">
-            <a href="/signin">Sign in</a>
+          <div className="w-1/6">
+            {user && (
+              <div className="text-white">
+                Login as:{" "}
+                <span className="hover:underline">
+                  <a onClick={logout}> {user.email} </a>
+                </span>
+              </div>
+            )}
           </div>
         </div>
       </nav>
