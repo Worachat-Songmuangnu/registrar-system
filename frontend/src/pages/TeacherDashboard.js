4b5d68d7994d5822c3c4290c1381e39114757556
Worachat Songmuangnu <6710110370@psu.ac.th>
2025-01-23 18:51:46 +0700

    feat: get announcement

--- frontend/src/pages/TeacherDashboard.js
+++ frontend/src/pages/TeacherDashboard.js
@@ -2,8 +2,27 @@ import { useNavigate } from "react-router-dom";
 import TeacherScoreCard from "../components/TeacherScoreCard";
 import { MagnifyingGlassIcon, PlusCircleIcon } from "@heroicons/react/24/outline";
 import HrLine from "../components/HrLine";
+import ax, { axData } from "../conf/ax";
+import { useEffect,useState } from "react";
+import { useAuth } from "../context/useAuth";
 export default function TeacherDashboard() {
+  const { user,isLoading } = useAuth()
+  const [data, setData] = useState(null);
   const navigate = useNavigate();
+  useEffect(() => {
+    const fetchData = async () => {
+      try{
+        const res = await ax.get(`/announcements?populate=Teacher&populate=scores&populate=student&filters[Teacher][username]=${user.username}`)
+        setData(res.data.data)
+        console.log(data)
+      }
+      catch(err) {
+        console.log(err)
+      }
+
+    }
+    fetchData()
+  },[isLoading])
   return (
     <div className="w-full flex flex-col mt-16">
       <div className="mb-12">
@@ -29,9 +48,24 @@ export default function TeacherDashboard() {
       </div>
       <HrLine />
       <div className="flex flex-col gap-5">
-        <TeacherScoreCard />
-        <TeacherScoreCard />
-        <TeacherScoreCard />
+
+      {data ? (
+          data.map((announcement) => (
+            <TeacherScoreCard
+              key={announcement.id}
+              title={announcement.Title}
+              name={announcement.subject_name}
+              id ={announcement.subject_id}
+              publish={announcement.publishedAt}
+              update={announcement.updatedAt}
+              scores={announcement.scores.data}
+              total={announcement.student.length}
+              max_score={announcement.max_score}
+            />
+          ))
+        ) : (
+          <p className="text-center text-gray-500">No announcements found.</p>
+        )}
       </div>
     </div>
   );
