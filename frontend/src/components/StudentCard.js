9e30adfd8ae7a2bad0dc7ac98530764f015e7f11
ณัฐกฤต มาชู <dungmachoo@gmail.com>
2025-01-25 15:07:35 +0000

    feat: add search filter system

--- frontend/src/components/StudentCard.js
+++ frontend/src/components/StudentCard.js
@@ -6,21 +6,25 @@ const StudentCard = (props) => {
   return (
     <div className="max-w-full p-6 bg-white border border-gray-200 rounded-lg shadow">
       <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
-        ({props.SubjectId}) {props.Subject}
+        {props.Title}
       </h5>
-
-      <div className="flex flex-col gap-4 justify-between">
-        <div className="flex flex-row gap-8 items-center">
-          <p className="mb-3 font-normal text-gray-700 text-3xl">
+      <div className="flex flex-col gap-4">
+        <p className="mb-3 font-normal text-gray-700 ">
+          Subject Name : {props.Subject}
+        </p>
+      </div>
+      <div className="flex flex-row gap-10">
+        <div className="flex flex-col">
+          <p className="mb-3 font-normal text-gray-700 text-xl">
             Score : {props.Score}/{props.MaxScore}
           </p>
-          <p className="mb-3 font-normal text-gray-700 text-xl">
-            Status : {props.Status}
-          </p>
-        </div>
-        <div className="flex flex-row gap-24">
           <p className="mb-3 font-normal text-gray-700 ">
             Date : {dayjs(props.Date).format("MMM D, YYYY h:mm A")}
+          </p>
+        </div>
+        <div className="flex flex-col">
+          <p className="mb-3 font-normal text-gray-700 text-xl">
+            Status : {props.Status}
           </p>
           <p className="mb-3 font-normal text-gray-700 ">
             Auditor : {props.Auditor}
